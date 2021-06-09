46a1c0c1066bda0589a1bb93a3e12aebb1d5d8c8
ayushbajpai790 <69970334+ayushbajpai790@users.noreply.github.com>
2021-06-09 18:24:56 +0530

    Create concate.py
    to concatenate two images horizontally as well as vertically

---
+++ concate.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+# load image
+pic1 = cv2.imread('black_stone.png')
+pic2 = cv2.imread('white-stone.png')
+# concate two images horizontally
+horiPic = np.concatenate((pic1,pic2),axis=1)
+
+cv2.imshow('Horizontal', horiPic)
+cv2.waitKey()
+cv2.destroyAllWindows()
+# concate two images horizontally
+VertiPic = np.concatenate((pic1,pic2),axis=0)
+
+cv2.imshow('Vertical', VertiPic)
+cv2.waitKey()
+cv2.destroyAllWindows()
